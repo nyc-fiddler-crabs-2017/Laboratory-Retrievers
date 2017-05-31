139fb616c79bc520b30dfbf86ec4866071d483f0
Alex Camacho <email": null>
2017-05-31 19:23:56 -0400

    Add null:false constraint to :title column

--- db/migrate/20170531145624_create_experiments.rb
+++ db/migrate/20170531145624_create_experiments.rb
@@ -1,7 +1,7 @@
 class CreateExperiments < ActiveRecord::Migration[5.0]
   def change
     create_table :experiments do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :result
       t.text :conclusion
       t.string :status, null: false, default: "Open"
