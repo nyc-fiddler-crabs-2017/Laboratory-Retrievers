028dd19344736faf758d07e08eb661e04716fdce
Alexandra Hill & Alex Persaud <pair+alexnhill.alexpersaud17@devbootcamp.com>
2017-05-31 11:40:11 -0400

    Remove unecessary controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,23 @@
 class UsersController < ApplicationController
-  def login
-  end
 
   def authenticate
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to '/welcome'
+      redirect_to "/users/#{current_user.id}"
     else
       redirect_to '/users/new'
     end
   end
 
-  def new
-  end
-
   def create
     user = User.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], username: params[:username], password: params[:password])
     session[:user_id] = user.id
-    redirect_to '/welcome'
+    redirect_to '/experiments'
   end
 
   def logout
     session.clear
-    redirect_to '/welcome'
+    redirect_to '/users/login'
   end
 end
