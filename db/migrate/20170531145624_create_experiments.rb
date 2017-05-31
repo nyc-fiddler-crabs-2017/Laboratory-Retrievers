8c56979258e898936e300e5d9777cfb30f9d6e38
Thomas Feng <email": null>
2017-05-31 15:59:34 -0400

    modified experiments to include title

--- db/migrate/20170531145624_create_experiments.rb
+++ db/migrate/20170531145624_create_experiments.rb
@@ -1,6 +1,7 @@
 class CreateExperiments < ActiveRecord::Migration[5.0]
   def change
     create_table :experiments do |t|
+      t.string :title
       t.text :result
       t.text :conclusion
       t.string :status, null: false, default: "Open"
