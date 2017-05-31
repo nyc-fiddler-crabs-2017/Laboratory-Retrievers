844eb0de80f47b4fba934949b89a3ff07a53a949
Thomas Feng <email": null>
2017-05-31 11:27:42 -0400

    Initial commit, added constraints. Added default for status

---
+++ db/migrate/20170531145624_create_experiments.rb
@@ -0,0 +1,13 @@
+class CreateExperiments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :experiments do |t|
+      t.text :result
+      t.text :conclusion
+      t.string :status, null: false, default: "Open"
+      t.references :experiment_proposal, foreign_key: true, null: false
+      t.references :user, foreign_key: true, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
