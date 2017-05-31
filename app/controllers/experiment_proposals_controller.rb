66b1511723f2a08b158df448b5129e832f16fea1
Alex Camacho <email": null>
2017-05-31 15:41:55 -0400

    Add #show action with @experiment_proposal variable added to be passed in to view file

--- app/controllers/experiment_proposals_controller.rb
+++ app/controllers/experiment_proposals_controller.rb
@@ -2,4 +2,8 @@ class ExperimentProposalsController < ApplicationController
   def index
     @experiment_proposals = ExperimentProposal.order('created_at DESC')
   end
+
+  def show
+    @experiment_proposal = ExperimentProposal.find_by(id: params[:id])
+  end
 end
