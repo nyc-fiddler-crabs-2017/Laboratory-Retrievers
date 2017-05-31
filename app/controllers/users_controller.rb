584f65ca3d869e617a06216f350d1b342883eb8c
Alexandra Hill & Alex Persaud <pair+alexnhill.alexpersaud17@devbootcamp.com>
2017-05-31 11:33:12 -0400

    Test user authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to '/posts'
+      redirect_to '/welcome'
     else
       redirect_to '/users/new'
     end
@@ -16,13 +16,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create(username: params[:username], password: params[:password])
+    user = User.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], username: params[:username], password: params[:password])
     session[:user_id] = user.id
-    redirect_to '/posts'
+    redirect_to '/welcome'
   end
 
   def logout
     session.clear
-    redirect_to '/posts'
+    redirect_to '/welcome'
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
-require BCrypt
 module UsersHelper
+  
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
   end
