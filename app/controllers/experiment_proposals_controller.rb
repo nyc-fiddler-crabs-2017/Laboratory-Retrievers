e152f9122e2fec58fae84095a8c26c043e0b6a69
Alex Camacho <email": null>
2017-05-31 16:11:53 -0400

    Add private strong params method, #create action that needs to be rewritten to account for user creating it

--- app/controllers/experiment_proposals_controller.rb
+++ app/controllers/experiment_proposals_controller.rb
@@ -6,4 +6,22 @@ class ExperimentProposalsController < ApplicationController
   def show
     @experiment_proposal = ExperimentProposal.find_by(id: params[:id])
   end
+
+  def new
+
+  end
+
+  def create
+      @experiment_proposal = ExperimentProposal.new(experiment_proposal_params)
+      if @experiment_proposal.save
+        redirect_to @experiment_proposal
+      else
+        render :new
+      end
+  end
+
+  private
+    def experiment_proposal_params
+      params.require(:experiment_proposal).permit(:title, :summary, :hyopothesis)
+    end
 end
