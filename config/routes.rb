301b538b308d49a39775a8f6b5078beb762e15fe
Thomas Feng <email": null>
2017-05-31 15:41:06 -0400

    update controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,15 @@ Rails.application.routes.draw do
   post '/users', to: 'users#create'
   post '/users/login', to: 'users#authenticate'
 
+  resources :experiment_proposal do
+    get '/experiment_proposal/:experiment_proposal_id/experiments', to: 'experiments#index'
+    get '/experiment_proposal/:experiment_proposal_id/experiments/new', to: 'experiments#new'
+    post '/experiment_proposal/:experiment_proposal_id/experiments', to: 'experiments#create'
+    get '/experiment_proposal/:experiment_proposal_id/experiments/:id', to: 'experiments#show'
+    get '/experiment_proposal/:experiment_proposal_id/experiments/:id/edit', to: 'experiments#edit'
+    put '/experiment_proposal/:experiment_proposal_id/experiments/:id', to: 'experiments#update'
+    delete '/experiment_proposal/:experiment_proposal_id/experiments/:id', to: 'experiments#destroy'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
