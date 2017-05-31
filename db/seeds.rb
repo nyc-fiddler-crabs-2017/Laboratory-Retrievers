2e0d60dec530030e6b09a80057966a73e275f48b
Brent Unkrich <bunkrich@gmail.com>
2017-05-31 11:49:59 -0400

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+user1 = User.create(first_name: "Alex", last_name: "Xela", username: "alex", email: "alex@laboratory_retrievers.edu", password: "password")
+user1.experiment_proposal = Experiment_Proposal.create(title: "Best Candy", summary: "We need to test all the candy in the store to find the best one.", hypothesis: "All candy is good.")
+user1.experiment = Experiment.create(experiment_proposal_id: 1, result: "Unknown Results", conclusion: "All candy is good.")
+
+10.times do
+  User.create(first_name: Faker::Internet.user_name, last_name: Faker::Hipster.word, username: Faker::HarryPotter.character, email: Faker::Internet.email, password: "password")
+end
+
+10.tmes do
+  Experiment_Proposal.create(title: Faker::HarryPotter.quote, summary: Faker::Hipster.sentence, hypothesis: Faker::Hipster.paragraph)
+end
+
+10.times do
+  x = Experiment.create(experiment_proposal_id: 1, user_id: 1, result: Faker::Hacker.verb, conclusion: "The conclusion is unknown at this time.")
+  x.comment = Comment.create(body: Faker::Hipster.sentence, user_id: 1)
+end
