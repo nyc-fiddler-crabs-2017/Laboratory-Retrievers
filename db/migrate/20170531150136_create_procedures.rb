6013dc0be0d20dd5cd45c17fed0e197bb1b46ae8
Alex Persaud <alex_persaud01@yahoo.com>
2017-05-31 11:57:14 -0400

    Update 20170531150136_create_procedures.rb

--- db/migrate/20170531150136_create_procedures.rb
+++ db/migrate/20170531150136_create_procedures.rb
@@ -1,6 +1,6 @@
 class CreateProcedures < ActiveRecord::Migration[5.0]
   def change
-    create_table :prodecures do |t|
+    create_table :procedures do |t|
       t.text :body, null: false
       t.integer :step, null: false
       t.references :experiment, foreign_key: true, null: false
