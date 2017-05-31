95f13e12cc416b980a665f261e211d061a1c7451
Thomas Feng <email": null>
2017-05-31 11:34:24 -0400

    Initial commit

--- db/migrate/20170531145624_create_experiments.rb
+++ db/migrate/20170531145624_create_experiments.rb
@@ -4,8 +4,7 @@ class CreateExperiments < ActiveRecord::Migration[5.0]
       t.text :result
       t.text :conclusion
       t.string :status, null: false, default: "Open"
-      t.references :experim
-      ent_proposal, foreign_key: true, null: false
+      t.references :experiment_proposal, foreign_key: true, null: false
       t.references :user, foreign_key: true, null: false
 
       t.timestamps null: false
