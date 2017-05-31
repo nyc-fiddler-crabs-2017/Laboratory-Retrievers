b44e57b9f48d16b1e7cd01b18e9517510477be2a
Brent Unkrich <bunkrich@gmail.com>
2017-05-31 15:34:47 -0400

    added rspec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,13 @@
-describe UserController do
+require 'rails_helper'
+
+describe "UserController" do
   let!(:user) {User.create!(first_name: "Alex", last_name: "Hill", username: "alexhill", email: "alex@hill.com", password: "password")}
 
 
   describe "GET#login" do
     it "responds with status code 200" do
       get :login
-      expect(response).to have_http_status
+      expect(response).to have_http_status 200
     end
   end
 
