86743651375c17250e1284bf684b1b1e8047667f
Brent Unkrich <bunkrich@gmail.com>
2017-06-01 10:22:11 -0400

    took out the validates for RESULT and CONCLUSION, so a user can create a new experiment without having to add that info

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -5,5 +5,5 @@ class Experiment < ApplicationRecord
   has_many :comments, as: :commentable
   has_many :observations, as: :observable
 
-  validates :result, :user, :conclusion, :status, :experiment_proposal, :presence => true
+  validates :user, :status, :experiment_proposal, :presence => true
 end
