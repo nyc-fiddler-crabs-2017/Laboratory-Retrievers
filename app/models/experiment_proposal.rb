4f12328d3e649eed26b856844de8bdbe8759c285
Brent Unkrich <bunkrich@gmail.com>
2017-06-01 16:30:17 -0400

    fixed the delete feature

--- app/models/experiment_proposal.rb
+++ app/models/experiment_proposal.rb
@@ -2,8 +2,8 @@ class ExperimentProposal < ApplicationRecord
   # belongs_to :proposer, class_name: "User"
   belongs_to :user
   has_many :experiments, dependent: :destroy
-  has_many :comments, as: :commentable
-  has_many :observations, as: :observable
+  has_many :comments, as: :commentable, dependent: :destroy
+  has_many :observations, as: :observable, dependent: :destroy
 
   validates :title, :summary, :hypothesis, :status, :user_id, :presence => true
 end
