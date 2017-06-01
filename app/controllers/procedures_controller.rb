bfc8fd9f4b88ed16f59665a881ff732e7264f872
Alexandra Hill <email": null>
2017-06-01 10:59:58 -0400

    Add initial procedure route methods

---
+++ app/controllers/procedures_controller.rb
@@ -0,0 +1,34 @@
+class ProceduresController < ApplicationController
+  def new
+    @experiment = Experiment.find(params[:experiment_id])
+
+    render 'procedures/_new'
+  end
+
+  def create
+    @experiment = Experiment.find(params[:experiment_id])
+    @procedure = @experiment.procedures.new(procedure_params)
+    if @procedure.save
+      redirect_to experiment_proposal_experiment_path(@experiment)
+    else
+      render 'procedures/_new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
+
+  private
+  def procedure_params
+    params.require(:procedure).permit(:body, :step, :experiment_id)
+  end
